1f5921ccc4435296ebc5a00e4008294a65c751cd
sundarraj33 <sundarraj.m33@gmail.com>
2025-01-10 01:03:16 +0530

    Update Edit Option

--- index.js
+++ index.js
@@ -4,24 +4,41 @@ var lists = document.querySelectorAll(".lists");
 var name = document.querySelector("#name");
 
 
+//  ---------------    Loaded Data from Localhost ---------------------
+
+
+document.addEventListener("DOMContentLoaded",()=>{   
+    var todos = [...JSON.parse(localStorage.getItem('todos'))];    
+    viewfun(todos);
+});
+
+
+//  ---------------    Loaded Data from Localhost ---------------------
+
 add_btn.addEventListener('click',()=>{
     var name = document.querySelector("#name");    
-
+        
+    localStorage.setItem('todos',
+        JSON.stringify(
+            [...JSON.parse(localStorage.getItem('todos') || "[]"),{todos:name.value}]
+        ));
+        
+        
+        output_field.innerHTML +=  `<div class='lists'>
+        <div class="paragraph">
+            <p>${name.value}</p>
+        </div>
+        <div class="buttons">
+            <div class='delbutton'>
+                <i class="fa-solid fa-trash" ></i>
+            </div>  
+            <div class='Editbutton'>
+                <i class="fa-solid fa-pen-to-square"></i>
+            </div>                                  
+        </button>                                        
+        </div>
+    </div>`; 
     
-    output_field.innerHTML +=  `<div class='lists'>
-                                    <div class="paragraph">
-                                        <p>${name.value}</p>
-                                    </div>
-                                    <div class="buttons">
-                                        <div class='delbutton'>
-                                            <i class="fa-solid fa-trash" ></i>
-                                        </div>  
-                                        <div class='Editbutton'>
-                                            <i class="fa-solid fa-pen-to-square"></i>
-                                        </div>                                  
-                                    </button>                                        
-                                    </div>
-                                </div>`;
 
                     var dels = document.getElementsByClassName("delbutton");    
                     for(var i=0;i<dels.length;i++){
@@ -43,3 +60,26 @@ add_btn.addEventListener('click',()=>{
 
 
 });
+
+
+function viewfun(todos){
+    todos.forEach((index)=>{
+        
+        output_field.innerHTML +=  `<div class='lists'>
+                                    <div class="paragraph">
+                                        <p>${index.todos}</p>
+                                    </div>
+                                    <div class="buttons">
+                                        <div class='delbutton'>
+                                            <i class="fa-solid fa-trash" ></i>
+                                        </div>  
+                                        <div class='Editbutton'>
+                                            <i class="fa-solid fa-pen-to-square"></i>
+                                        </div>                                  
+                                    </button>                                        
+                                    </div>
+                                </div>`; 
+    })
+    
+    
+}
