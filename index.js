15c921748f480df0e2023bea6cff36c19f240f4e
sundarraj33 <sundarraj.m33@gmail.com>
2025-01-09 22:25:04 +0530

    Todo List

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 var add_btn = document.getElementById('Add');
 var output_field = document.querySelector(".output_field");
-
-
-
 var lists = document.querySelectorAll(".lists");
+var name = document.querySelector("#name");
 
 
 add_btn.addEventListener('click',()=>{
     var name = document.querySelector("#name");    
-   
+
+    
     output_field.innerHTML +=  `<div class='lists'>
                                     <div class="paragraph">
                                         <p>${name.value}</p>
                                     </div>
-                                    <div class="buttons">
-                                        <i class="fa-solid fa-trash dels" onClick='del()' id=''></i>
-                                        <i class="fa-solid fa-pen-to-square" onClick='edit()'></i>
+                                    <div class="buttons"><i class="fa-solid fa-trash del"></i></button>                                        
                                     </div>
                                 </div>`;
 
-    // remove(name.value);
+                    var dels = document.getElementsByClassName("buttons");    
+                    for(var i=0;i<dels.length;i++){
+                        dels[i].onclick = function(){
+                            this.parentNode.remove();
+                        }
+                    }
+   
     name.value ='';
 
 
 });
-function del(){
-    var output_fields = output_field.children;
-    // console.log(output_fields)
-    for(var i=0;i<output_fields.length;i++){
-        output_fields[i].onClick = function(){
-            this.parentNode.remove();
-        }
-    }
-}
